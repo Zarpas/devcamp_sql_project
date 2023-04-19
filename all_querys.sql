6e19403b5dc02e357ad96dfddc933ffc4c3ada29
Igor Martín <imartin1974@gmail.com>
2023-04-19 11:06:11 +0200

    Update all_querys.sql

--- all_querys.sql
+++ all_querys.sql
@@ -3,7 +3,7 @@ USE devcamp_university_schema_test;
 
 -- The average grade that is given by each professor
 SELECT 
-	AVG(grades.grades_grade), 
+    AVG(grades.grades_grade), 
     courses.courses_professors_id, 
     p.professors_name
 FROM grades
@@ -17,7 +17,7 @@ ORDER BY courses.courses_professors_id;
 
 -- the top grades for each student
 SELECT 
-	MAX(g.grades_grade) AS `best grade`, 
+    MAX(g.grades_grade) AS `best grade`, 
     g.grades_students_id,
     s.students_id,
     s.students_name, 
@@ -31,9 +31,9 @@ ORDER BY `best grade` DESC;
 
 -- group students by the courses taht they are enrolled in
 SELECT 
-	COUNT(*) AS student_number, 
-	grades_courses_id, 
-	courses.courses_subject 
+    COUNT(*) AS student_number, 
+    grades_courses_id, 
+    courses.courses_subject 
 FROM grades 
 JOIN courses
 ON grades.grades_courses_id = courses.courses_id
@@ -43,7 +43,7 @@ GROUP BY grades_courses_id;
 -- Create a summary report of courses and their average grades, sorted by the most challenging 
 -- course (course with the lowest average grade) to the easiest course
 SELECT 
-	AVG(g.grades_grade) AS average_grade, 
+    AVG(g.grades_grade) AS average_grade, 
     g.grades_courses_id,
     c.courses_subject
 FROM grades AS g 
@@ -55,9 +55,9 @@ ORDER BY average_grade ASC;
 
 -- Finding which student and professor have the most courses in common
 SELECT 
-	g.grades_students_id,
+    g.grades_students_id,
     s.students_name,
-	COUNT(g.grades_courses_id) AS courses_count, 
+    COUNT(g.grades_courses_id) AS courses_count, 
     c.courses_professors_id,
     p.professors_name
 FROM grades AS g
